6efd68b9bf15315f639c2cb7f5a89089e01cdc90
Joalbert <joalbertgonzalez@gmail.com>
2019-06-09 00:50:37 -0500

    added state and componentDidMount

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,30 +5,63 @@ import './styles/Card.css';
 // assets
 import circlesImg from '../images/circles.png';
 
-// class Card extends React.Component {
-//   render() {
-//     return <h1>Test</h1>
-//   }
-// }
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    const {title, description, img, leftColor, rightColor} = props;
 
-const Card = ({title, description, img, leftColor, rightColor}) => (
-  <div className="card mx-auto Fitness-Card"
-    style={{
-      backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`,
-    }}
-  >
-    <div className="card-body">
-      <div className="row center">
-        <div className="col-6">
-          <img src={img} className="float-right"/>
-        </div>
-        <div className="col-6 Fitness-Card-Info">
-          <h1>{title}</h1>
-          <p>{description}</p>
+    this.state = {title, description, img, leftColor, rightColor};
+  }
+
+componentDidMount() {
+  setTimeout(() => {
+    this.setState({
+      img: ""
+    })
+  }, 5000);
+} 
+
+  render() {
+    return (
+      <div className="card mx-auto Fitness-Card"
+        style={{
+          backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${this.state.leftColor}, ${this.state.rightColor})`,
+        }}
+      >
+        <div className="card-body">
+          <div className="row center">
+            <div className="col-6">
+              <img src={this.state.img} className="float-right"/>
+            </div>
+            <div className="col-6 Fitness-Card-Info">
+              <h1>{this.state.title}</h1>
+              <p>{this.state.description}</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
+
+// const Card = ({title, description, img, leftColor, rightColor}) => (
+//   <div className="card mx-auto Fitness-Card"
+//     style={{
+//       backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`,
+//     }}
+//   >
+//     <div className="card-body">
+//       <div className="row center">
+//         <div className="col-6">
+//           <img src={img} className="float-right"/>
+//         </div>
+//         <div className="col-6 Fitness-Card-Info">
+//           <h1>{title}</h1>
+//           <p>{description}</p>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// )
 
 export default Card;
