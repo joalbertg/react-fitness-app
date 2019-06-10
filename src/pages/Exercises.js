bc66d1d9b782aafd22d316fffe4dca283cb3a52e
Joalbert <joalbertgonzalez@gmail.com>
2019-06-09 23:41:59 -0500

    remove data faker and connect with api-fitness

--- src/pages/Exercises.js
+++ src/pages/Exercises.js
@@ -5,7 +5,7 @@ import ExerciseList from '../components/ExerciseList';
 import AddExercise from '../components/AddExercise';
 
 // info faker
-import exerciseData from '../faker/exercises.json';
+// import exerciseData from '../faker/exercises.json';
 
 class Exercises extends React.Component {
   // constructor(props) {
@@ -16,18 +16,42 @@ class Exercises extends React.Component {
   // }
 
   // by Babel, without constructor method
-  state = { exerciseData }
-  render = () => (
-    <>
-      <Welcome
-        username='Joalbert'
-      />
-      <ExerciseList
-        exerciseData={this.state.exerciseData}
-      />
-      <AddExercise />
-    </>
-  )
+  // state = { exerciseData }
+  state = { exerciseData: [], error: false }
+
+  componentDidMount = async () => {
+    await this.fetchExercises()
+  }
+
+  fetchExercises = async () => {
+    try {
+      let res = await fetch('http://localhost:8000/api/exercises');
+      this.setState({
+        exerciseData: await res.json()
+      })
+    } catch(error) {
+      this.setState({
+        error: true
+      })
+    }
+  }
+
+  render = () => {
+    return (
+    this.state.error ?
+      <h1>Server Error!</h1>
+    :
+      <>
+        <Welcome
+          username='Joalbert'
+        />
+        <ExerciseList
+          exerciseData={this.state.exerciseData}
+        />
+        <AddExercise />
+      </>
+    )
+  }
 }
 
 export default Exercises;
