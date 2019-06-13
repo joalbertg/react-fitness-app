fddc71dadeaedec0ea6ee9d7a280e125e9cbea22
Joalbert <joalbertgonzalez@gmail.com>
2019-06-13 00:00:31 -0500

    added useState hook

--- src/pages/ExercisesContainer.js
+++ src/pages/ExercisesContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Exercises from './Exercises';
 import Loading from '../components/Loading';
@@ -7,49 +7,32 @@ import FatalError from './500';
 // info faker
 // import exerciseData from '../faker/exercises.json';
 
-class ExercisesContainer extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     exerciseData
-  //   }
-  // }
+const fetchExercises = async (setLoading, setExerciseData, setError) => {
+  try {
+    let res = await fetch('http://localhost:8000/api/exercises');
 
-  // by Babel, without constructor method
-  // state = { exerciseData }
-  state = {
-    exerciseData: [],
-    error: false,
-    loading: true
+    setLoading(false);
+    setExerciseData(await res.json());
+  } catch(error) {
+    setLoading(false);
+    setError(true);
   }
+}
 
-  componentDidMount = async () => {
-    await this.fetchExercises()
-  }
+const ExercisesContainer = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [exerciseData, setExerciseData] = useState([]);
 
-  fetchExercises = async () => {
-    try {
-      let res = await fetch('http://localhost:8000/api/exercises');
-      this.setState({
-        exerciseData: await res.json(),
-        loading: false
-      })
-    } catch(error) {
-      this.setState({
-        error: true,
-        loading: false
-      })
-    }
-  }
-
-  render = () => (
-    this.state.loading ?
+  fetchExercises(setLoading, setExerciseData, setError);
+  return (
+    loading ?
       <Loading />
     :
-      this.state.error ?
+      error ?
         <FatalError />
       :
-        <Exercises exerciseData={this.state.exerciseData} />
+        <Exercises exerciseData={exerciseData} />
   )
 }
 
